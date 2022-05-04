7cf95b1f8de68d9c28b116053e308ce9b597cc66
bergony.bandeira <bergony@hotmail.com>
2022-05-04 13:31:41 -0300

    Teste Commit

--- src/main/java/ufrn/br/web/controllers/PessoaController.java
+++ src/main/java/ufrn/br/web/controllers/PessoaController.java
@@ -1,16 +1,28 @@
 package ufrn.br.web.controllers;
 
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import ufrn.br.web.model.Pessoa;
+import ufrn.br.web.services.PessoaService;
+
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
+@Slf4j
 public class PessoaController {
+
+    @Autowired
+    private PessoaService pessoaService;
 
     // inject via application.properties
     @Value("${welcome.message}")
@@ -41,5 +53,16 @@ public class PessoaController {
         model.addAttribute("message", name);
 
         return "welcome"; //view
+    }
+
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public ResponseEntity<?> handleForm(@RequestParam("firstName") String firstName,
+                                        @RequestParam("firstName") String lastName, @RequestParam("pessoa")
+                                                Pessoa pessoa) {
+        log.info("first Name : {}", firstName);
+        log.info("Last Name : {}", lastName);
+        log.info("pessoa: {}", pessoa);
+        return ResponseEntity.ok().body(firstName);
     }
 }
