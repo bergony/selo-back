4a43fdf87e3f9c7ff8db9ef4ee8e6e2ef3c1d0ff
marlusmarcos <marlusufrn@gmail.com>
2022-10-05 15:35:20 -0300

    teste de pessoa para hacktube fast

--- src/test/java/ufrn/br/web/WebApplicationTests.java
+++ src/test/java/ufrn/br/web/WebApplicationTests.java
@@ -5,9 +5,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WebApplicationTests {
+	@Autowired
+    private PessoaRepository pessoaRepository;
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	public void testeUsuario () {
+		Pessoa pessoa = new Pessoa ();
+		AssertEquals(pessoaRepository.savePessoa (pessoa), pessoa);
+	}
+	//teste para verificar se está sendo feito corretamente o save de pessoa
 
 }
