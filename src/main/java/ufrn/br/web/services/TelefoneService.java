3243c94892e06800d0596c7e54d0b4092483f75a
marlusmarcos <marlusufrn@gmail.com>
2022-11-01 15:14:25 -0300

    crud telefone no service

---
+++ src/main/java/ufrn/br/web/services/TelefoneService.java
@@ -0,0 +1,63 @@
+package ufrn.br.web.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ufrn.br.web.model.Telefone;
+import ufrn.br.web.repositoreis.TelefoneRepository;
+
+public class TelefoneService {
+	
+	@Autowired
+	private TelefoneRepository telefoneRepository;
+	
+	public Telefone cadastrarTelefone (Telefone telefone) throws Exception {
+		if (telefone.getNumero() == null) {
+			throw new Exception("ERRO! Número está vazio!");
+		}
+		return telefoneRepository.save(telefone);
+	}
+	
+	public Optional <Telefone> buscarPorId (Integer id) {
+		Optional <Telefone> telefone = telefoneRepository.findById(id);
+		return telefone;
+	}
+	
+	public Telefone delete (Integer id) {
+		Optional <Telefone> telefone = telefoneRepository.findById(id);
+		if (telefone.isPresent()) {
+			telefoneRepository.delete(telefone.get());
+			return telefone.get();
+		}
+		return null;
+	}
+	
+	public Telefone editTelefone (Integer id, Telefone telefone) {
+		
+		Telefone oldTelefone = buscarPorId(id).get();
+		if (oldTelefone == null) {
+			return null;
+		}
+		oldTelefone.setNumero(telefone.getNumero());
+		telefoneRepository.save(oldTelefone);
+		
+		
+		return telefone;
+		
+	} 
+	
+	public List<Telefone> buscarPorPessoa (Integer id) throws Exception {
+		List <Telefone> telefones = telefoneRepository.findByPessoa(id);
+		if (telefones.isEmpty()) {
+			throw new Exception ("Não há telefone para o usuário com id: " + id + " !!");
+		}
+		return telefones; 
+	}
+	
+	
+	
+	
+
+}
