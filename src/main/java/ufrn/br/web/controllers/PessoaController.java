519ec351b871a9bff129e27b8e2099301f84afec
marlusmarcos <marlusufrn@gmail.com>
2022-11-10 15:52:06 -0300

    Dtos de endereco, pessoa e telefone com validacoes ok

--- src/main/java/ufrn/br/web/controllers/PessoaController.java
+++ src/main/java/ufrn/br/web/controllers/PessoaController.java
@@ -7,6 +7,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -51,11 +54,16 @@ public class PessoaController {
 //    }
 
     @GetMapping ("{id}")
-    public Pessoa findById (@PathVariable Integer id) {
-    	return pessoaService.findPessoalByID(id);
+    public ResponseEntity<Pessoa> findById (@PathVariable Integer id) {
+    	Pessoa p = pessoaService.findPessoalByID(id);
+    	if (p != null) {
+    		return ResponseEntity.ok(p);
+    	}
+    	return ResponseEntity.notFound().build();
     }
     
     @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
     public Pessoa cadastrarPessoa (@RequestBody Pessoa pessoa) {
     	return pessoaService.savePessoa(pessoa);
     }
